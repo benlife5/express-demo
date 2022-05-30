c767f52c6a4ccc66c8b026a52e563ad0d766084d
Ben Life <77246839+benlife5@users.noreply.github.com>
2022-05-30 11:15:32 -0400

    added firebase support

--- api/routes/demo.js
+++ api/routes/demo.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const router = express.Router()
+const db = require("./firebase")
 
-router.get("/info", (req, res, next) => {
+const {getDocs, collection} = require("firebase/firestore")
+
+router.get("/info", async (req, res, next) => {
   console.log(req.query)
+  const docs = await getDocs(collection(db, "responses"))
+  console.log(docs)
   res.send("API is working properly")
 })
 
